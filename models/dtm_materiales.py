ce8b0ef04c5062bfe6dbb75fc9afa5e82f0a1863
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2023-12-22 11:37:54 -0600

    Se agrega entradas y salidas para modificar el stock

--- models/dtm_materiales.py
+++ models/dtm_materiales.py
@@ -8,7 +8,28 @@ class Materiales(models.Model):
     calibre_id = fields.Many2one("dtm.calibre.material",string="CALIBRE")
     largo_id = fields.Many2one("dtm.largo.material",string="LARGO", default="0")
     ancho_id = fields.Many2one("dtm.ancho.material",string="ANCHO", default="0")
-    cantidad = fields.Integer(string="CANTIDAD")
+    descripcion = fields.Text(string="Descripción")
+    entradas = fields.Integer(string="Entradas")
+    salidas = fields.Integer(string="Salidas")
+    cantidad = fields.Integer(string="Stock")
+    
+    @api.onchange("entradas")
+    def _onchange_entradas(self):
+        for record in self:
+            record.cantidad += record.entradas
+            record.cantidad = 0
+
+    def accion_salidas(self):
+        for record in self:
+            record.cantidad -= record.salidas
+            record.salidas = 0
+
+
+
+
+
+
+
 
    
         
