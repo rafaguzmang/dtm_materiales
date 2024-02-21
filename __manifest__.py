41c160ee9a5a6e8c97c5771757c6750c6de24a8b
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-02-21 16:18:39 -0600

    servidor

--- __manifest__.py
+++ __manifest__.py
@@ -5,12 +5,9 @@
         'security/ir.model.access.csv',
         'views/dtm_materiales_views.xml',
         'views/dtm_perfiles_views.xml',
-<<<<<<< HEAD
+        'views/dtm_tubos_views.xml',
 
         #menú
-=======
-        'views/dtm_tubos_views.xml',
->>>>>>> 70f1fe2f54691e86a1d053e984dd25579c6ae590
         'views/dtm_menu.xml'
     ]
 }
